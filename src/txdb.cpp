497427ec18f05ed05cf5484b9330f915e60ec2e0
Rjected <kidscline01@gmail.com>
2018-06-06 20:23:36 -0400

    Added comment to ReadVoteCount in txdb.cpp This code needs to be fixed so it compiles and then will be tested

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -167,6 +167,7 @@ size_t CCoinsViewDB::EstimateSize() const
 CBlockTreeDB::CBlockTreeDB(size_t nCacheSize, bool fMemory, bool fWipe) : CDBWrapper(gArgs.IsArgSet("-blocksdir") ? GetDataDir() / "blocks" / "index" : GetBlocksDir() / "index", nCacheSize, fMemory, fWipe) {
 }
 
+// This declaration isn't working
 bool CCoinsViewDB::ReadVoteCount(const std::string addr, int& nVotes) { return true; } //TODO: finish this read method
 
 // TODO: Implement diff-wise vote counting at some point.
